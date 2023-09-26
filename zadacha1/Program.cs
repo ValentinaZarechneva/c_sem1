0b2b20a15f13551dedaafb3c3a9ca6581fb5ba04
Valentina Zarechneva <valvas_07@mail.ru>
2023-09-26 22:45:48 +0300

    задача1, квадрат числа

---
+++ zadacha1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n + 5);*/  //преобразование строки в число
+// ctrl+/
+// ctrl+k, ctrl+u
+
+
+
+/*Напишите программу, которая на вход принимает
+ число и выдает его квадрат (число умноженное 
+ // на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49*/
+
+Console.Clear();
+Console.Write("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n * n);
+
+
+
+
